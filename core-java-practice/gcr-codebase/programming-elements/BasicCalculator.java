12b9ec7a0dd6b222b77c79ef6ebab2134e696254
hritiksingh66 <hritiksinghrajput05162@gmail.com>
2025-12-18 23:53:52 +0530

    [Hritik Kumar] added BasicCalculator.java program under programming-elements

---
+++ core-java-practice/gcr-codebase/programming-elements/BasicCalculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Taking user input
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        // Arithmetic operations
+        double addition = num1 + num2;
+        double subtraction = num1 - num2;
+        double multiplication = num1 * num2;
+        double division = num1 / num2;
+
+        // Printing results
+        System.out.println(
+            "The addition, subtraction, multiplication and division value of 2 numbers "
+            + num1 + " and " + num2 + " is "
+            + addition + ", "
+            + subtraction + ", "
+            + multiplication + ", and "
+            + division
+        );
+
+        sc.close();
+    }
+}
