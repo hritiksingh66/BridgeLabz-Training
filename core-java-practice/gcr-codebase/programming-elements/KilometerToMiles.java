adfe19beedcacfdf73e935bf8336b712c4b8f3a8
hritiksingh66 <hritiksinghrajput05162@gmail.com>
2025-12-18 11:15:56 +0530

    [Hritik Kumar] added KilometerToMiles.java program under programming-elements

---
+++ core-java-practice/gcr-codebase/programming-elements/KilometerToMiles.java
@@ -0,0 +1,16 @@
+public class KilometerToMiles {
+
+  public static void main(String[] args) {
+
+    // given distance in kilometers
+    double dist_in_km = 10.8;
+
+    // Converting km into miles using 1km = 1.6 miles
+    double dist_in_miles  = dist_in_km * 1.6 ;
+
+
+    // Print result as distance in miles
+
+    System.out.println("Distance in Miles is " + dist_in_miles);
+  }
+}
